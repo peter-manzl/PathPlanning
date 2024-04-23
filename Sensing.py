f566b173deac1c12e7e02f53676b6aa8f3240e82
Peter Manzl <peter.manzl@uibk.ac.at>
2024-04-23 13:25:46 +0200

    implemented finding circles intersection point

--- Sensing.py
+++ Sensing.py
@@ -67,7 +67,7 @@ class Robot():
          else: 
             self.env = env
             
-    def checkRectangles(self, phi, length, verbose=False): 
+    def checkSensing(self, phi, length, verbose=False): 
         pRobot = [self.x[-1], self.y[-1]]
         pMaxSensing = [self.x[-1] + (length * np.cos(phi)), self.y[-1] + length*np.sin(phi)]
         # point1 = pRobot
@@ -108,8 +108,8 @@ class Robot():
             # print('phi: ', phi)
             # check if one of the following obstacles is seen by the sensor
             # self.checkBoundary(phi, self.sensorLidar['range'])
-            self.checkRectangles(phi, self.sensorLidar['range'], verbose=verbose)
-            self.checkCircles(phi, self.sensorLidar['range'])
+            self.checkSensing(phi, self.sensorLidar['range'], verbose=verbose)
+            # self.checkCircles(phi, self.sensorLidar['range'])
             
     
         
@@ -120,44 +120,134 @@ myRobot = Robot(2, 2.5, env=myEnv)
 
 myEnv.plot(myRobot)
 myRobot.sense(True)
-sys.exit()
+# sys.exit()
 
 #%% 
+# import math
 
-x0 = 5
-y0 = 5
-dy = 2
-dx = 20
-# plt.plot([x0, x0 + dx], [y0, y0], label='1')
-# plt.plot([x0+dx, x0 + dx],  [y0, y0+dy], label='2')
-# plt.plot([x0, x0], [y0, y0+dy], label='3')
-# plt.plot([x0, x0+dx], [y0+dy, y0+dy], label='4')
-plt.legend()
-
-
-
+def find_circle_line_intersection(h, k, r, m, b):
+    # Coefficients in the quadratic equation Ax^2 + Bx + C = 0
+    A = 1 + m**2
+    B = 2 * (m * b - m * k - h)
+    C = h**2 + b**2 - 2 * b * k + k**2 - r**2
+    print(h,k,r,m,b)
+    # Calculate the discriminant
+    discriminant = B**2 - 4 * A * C
     
+    if discriminant < 0:
+        return None
+    else:
+        # Calculate x coordinates of the intersection points
+        sqrt_discriminant = np.sqrt(discriminant)
+        x1 = (-B + sqrt_discriminant) / (2 * A)
+        x2 = (-B - sqrt_discriminant) / (2 * A)
+        
+        # Corresponding y coordinates
+        y1 = m * x1 + b
+        y2 = m * x2 + b
+        
+        if discriminant == 0:
+            return [[x1, y1]]
+        else:
+            return [[x1, y1], [x2, y2]]
+        
+def find_circle_line_intersection_point(point1, point2, xCircle, yCircle, r):
+    k = (point2[1] - point1[1]) / (point2[0] - point1[0])
+    b = point1[1] - k * point1[0]
+    
+    # Coefficients in the quadratic equation Ax^2 + Bx + C = 0
+    A = 1 + k**2
+    B = 2 * (k * b - k * yCircle - xCircle)
+    C = xCircle**2 + b**2 - 2 * b * yCircle + yCircle**2 - r**2
+    
+    # Calculate the discriminant
+    discriminant = B**2 - 4 * A * C
+    
+    if discriminant < 0:
+        return None
+    else:
+        # Calculate x coordinates of the intersection points
+        sqrt_discriminant = np.sqrt(discriminant)
+        x1 = (-B + sqrt_discriminant) / (2 * A)
+        x2 = (-B - sqrt_discriminant) / (2 * A)
+        
+        # Corresponding y coordinates
+        y1 = k * x1 + b
+        y2 = k * x2 + b
+        # print(discriminant, A, B)
+        if discriminant == 0:
+            return [x1, y1]
+        else:
+            if True: 
+                r1 = [x1 - point1[0],  y1 - point1[1]]
+                r2 = [x2 - point1[0],  y2 - point1[1]]
+                rPoints = [point2[0] - point1[0], point2[1] - point1[1]]
+                # phi = np.arctan2(point2[1] - point1[1],point2[0] - point1[0])
+                
+                e = np.array([point2[0] - point1[0], point2[1] - point1[1]])
+                e = e / np.linalg.norm(e)     # unity vector
+                
+                
+                dir1 = (r1 / e)[0]
+                dir2 = (r2 / e)[0]
+                
+                # check if intersection is not between but outside of points
+                if np.linalg.norm(r1) > np.linalg.norm(rPoints): 
+                    dir1 = -1 
+                if np.linalg.norm(r2) > np.linalg.norm(rPoints): 
+                    dir2 = -1  
+                    
+                if dir1 > 0 and dir2 < 0:
+                    return [x1,y1]
+                elif dir1 > 0 and dir2 > 0: 
+                    # both in direction of 2:
+                    if dir1 > dir2: # further away!
+                        return [x2, y2]
+                    else:
+                        return [x1,y1]
+                elif dir1 < 0 and dir2 > 0: 
+                    return [x2,y2]
+                else: 
+                    return None
+            else: 
+                
+            # plt.plot([point1[0], point1[0]+e[0]], [point1[1], point1[1]+e[1]], 'o--')
+            # print('l1:', l1, '\nl2:', l2, '\ne:', e)
+                return [[x1, y1], [x2, y2]]
 
-x0_, y0_, dx_, dy_ = 10,2,5,5
-point1 = (x0, y0)
-point2 = (x0+dx, y0+dy)
-point3 = (x0_, y0_)
-point4 = (x0_+dx_, y0+y0_)
-xC, u = line_intersection(point1, point2, point3, point4)
-plt.plot([point1[0], point2[0]], [point1[1], point2[1]])
-plt.plot([point3[0], point4[0]], [point3[1], point4[1]], '--')
-plt.plot(xC[0], xC[1], 'x')
+# Example usage
+# h, k, r = 0, 0, 5  # Circle center at (0,0) and radius 5
+# m, b = 1, 0       # Line y = xl0 = 10
+# xC = find_circle_line_intersection(h, k, r, m, b)
+phi = np.linspace(0, np.pi*2, 1001)
+np.random.seed(42)
+for i in range(10): 
+    plt.figure()
+    
+    r = 5* (np.random.random()-0.5) * 2
+    p1 = (np.random.random(2)-0.5) * 10 # [-2, -10]  
+    p2 = (np.random.random(2)-0.5) * 10 #[10, 10]
+    xCircleM, yCircleM = 0,0
+    xC = find_circle_line_intersection_point(p1, p2, xCircleM, yCircleM, r)
 
-# np.array(point3) - xC
-
-#%% 
-point1 = (1, 1)
-point2 = (4, 4)
-point3 = (1, 8)
-point4 = (2, 0)
-intersection_point = line_intersection(point1, point2, point3, point4)
-if intersection_point:
-    plt.plot(intersection_point[0], intersection_point[1], 'x')
-    plt.plot([point1[0], point2[0]], [point1[1], point2[1]])
-    plt.plot([point3[0], point4[0]], [point3[1], point4[1]], '--')
+    # plt.plot([h, h+l0*m], [k, k+l0*m])
+    # plt.plot([h, h-l0*m], [k, k-l0*m])
+    
+    xCircle = r* np.cos(phi) + xCircleM
+    yCircle = r* np.sin(phi) + yCircleM
+    plt.plot(xCircle, yCircle, '-')
+    plt.axis('equal')
+    # for data in xC: 
+        # plt.plot(data[0], data[1], 'x')
+    if not(xC is None): 
+        # for data in xC: 
+        plt.plot(xC[0], xC[1], 'o')
+        plt.title('contact at: {}, {}'.format(round(xC[0], 3), round(xC[1], 3)))
+    else: 
+        plt.title('no contact')
+    plt.plot([p1[0],p2[0]], [p1[1],p2[1]], 'x-')
+    plt.text(p1[0]+0.1, p1[1], 'p1')
+    plt.grid()
+    # print("sC == xC2: ", xC == xC2)
+    
     
